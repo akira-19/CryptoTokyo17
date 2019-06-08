34337adb7670daafaa7eb3de8e0e7bcc6c1f4d2d
Nanamachi <7machi@nanamachi.net>
2019-06-08 20:20:05 +0900

    add frontend/pages/reserve.vue: add reservation table

---
+++ frontend/pages/reserve.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <reserve-table />
+  </div>
+</template>
+
+<script>
+import ReserveTable from '~/src/components/ReserveTable.vue'
+
+export default {
+  components: {
+    ReserveTable
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/ReserveColumn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="column">
+    <reserve-cell v-for="i in 48" :key="i" />
+  </div>
+</template>
+
+<script>
+import ReserveCell from './ReserveCell.vue'
+
+export default {
+  components: {
+    ReserveCell
+  }
+}
+</script>
+
+<style scoped>
+.column {
+  display: flex;
+  flex-direction: column;
+}
+</style>
