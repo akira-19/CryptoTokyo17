26f5d81c1d751b207c732c5279e3493a2d1a51e4
yosuke.aramaki <gnx.vw903@gmail.com>
2019-06-09 09:54:12 +0900

    contract

--- backend/contracts/Booking.sol
+++ backend/contracts/Booking.sol
@@ -7,7 +7,7 @@ import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract Booking is ERC721Full, Ownable{
 
     constructor(string memory name, string memory symbol) ERC721Full(name, symbol) public {
-
+   
     }
 
     struct reserveInfo {
@@ -24,7 +24,7 @@ contract Booking is ERC721Full, Ownable{
         address restaurant;
         uint16 seatNum;
         uint16 capacity;
-        uint reservationFee;
+        uint reservationFee;   
     }
 
     reserveInfo[] public reservesInfo;
@@ -34,41 +34,40 @@ contract Booking is ERC721Full, Ownable{
     uint tokenId = 1;
     bytes32 hashForRegister = '';
 
-    function transferEther(address payable _to) public payable{
-        uint value = msg.value;
-        _to.transfer(value);
+    function transferTo(address payable _dest) public payable {
+        _dest.transfer(msg.value);
     }
 
-    function registerSeat(address payable _resutaurantAddress, uint16 _seatNum, uint32 _yaerAndMonth, uint32 _time, uint _cost) public payable{
+    function registerSeat(address payable _resutaurantAddress, uint16 _seatNum, uint32 _yaerAndMonth, uint32 _time, uint _cost) public payable {
         super._mint(msg.sender, tokenId);
-        /* reservesInfo.push(reserveInfo(tokenId, _resutaurantAddress, _seatNum, _yaerAndMonth, _time, _cost)); */
+        //reservesInfo.push(reserveInfo(tokenId, _resutaurantAddress, _seatNum, _yaerAndMonth, _time, _cost));
         hashForRegister = keccak256(abi.encode(_seatNum, _yaerAndMonth, _time));
         oneMonthSeatInfo[hashForRegister] = reserveInfo(tokenId, _resutaurantAddress, _seatNum, _yaerAndMonth, _time, _cost, false);
-        _resutaurantAddress.transfer(_cost);
+        _resutaurantAddress.transfer(msg.value);
         tokenId++;
     }
 
-    function onSale(address _resutaurantAddress, uint16 _seatNum, uint32 _yaerAndMonth, uint32 _time, uint _cost) public {
-        oneMonthSeatInfo[hashForRegister] = reserveInfo(tokenId, _resutaurantAddress, _seatNum, _yaerAndMonth, _time, _cost, true);
-        super.transferFrom(msg.sender, address(this), tokenId);
+    function onSale(uint _tokenId, address _resutaurantAddress, uint16 _seatNum, uint32 _yaerAndMonth, uint32 _time, uint _cost) public {
+        oneMonthSeatInfo[hashForRegister] = reserveInfo(_tokenId, _resutaurantAddress, _seatNum, _yaerAndMonth, _time, _cost, true);
+        super.transferFrom(msg.sender, address(this), _tokenId);
     }
 
-    function buySeat(address payable _to, uint tokenId, uint _amount) public {
-        this.transferFrom(address(this), msg.sender, tokenId);
-        _to.transfer(_amount);
+    function buySeat(address payable _to, uint _tokenId ) public payable {
+        this.transferFrom(address(this), msg.sender, _tokenId);
+        _to.transfer(msg.value);
     }
 
-    function getBackSeat(address payable _to, uint tokenId, uint _amount) public {
-        super.transferFrom(msg.sender, _to, tokenId);
-        _to.transfer(_amount);
+    function getBackSeat(address payable _to, uint _tokenId ) public payable{
+        super.transferFrom(msg.sender, _to, _tokenId);
+        _to.transfer(msg.value);
     }
 
     function getoneMonthSeatInfo(uint16 _seatNum, uint32 _yaerAndMonth, uint32 _time) public view returns (reserveInfo memory) {
         return oneMonthSeatInfo[keccak256(abi.encode(_seatNum, _yaerAndMonth, _time))];
     }
 
-    function createSeat(address _restaurant, uint16 _seatNum, uint16 _capacity, uint reservationFee) public {
-        seatInformation[_restaurant][_seatNum] = seatInfo( _restaurant, _seatNum, _capacity, reservationFee);
+    function createSeat(address _restaurant, uint16 _seatNum, uint16 _capacity, uint _reservationFee) public {
+        seatInformation[_restaurant][_seatNum] = seatInfo( _restaurant, _seatNum, _capacity, _reservationFee);
     }
 
     function getSeatInfo(address _restaurant, uint16 _seatNum) public view returns (seatInfo memory) {
